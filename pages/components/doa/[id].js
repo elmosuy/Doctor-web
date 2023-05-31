2f2d36f524110115295a7d5530f96fa8432bdedd
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-05-31 21:13:05 +0300

    Add files via upload

--- pages/components/doa/[id].js
+++ pages/components/doa/[id].js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import data from "../../api/datas.json"
 import data2 from "../../api/headers.json"
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default function Alaj() {
     const router = useRouter()
@@ -13,11 +14,14 @@ export default function Alaj() {
 
   return (
     <div  className='alaj' >
+       <Link href="/components/doa"><span className='right' ><svg width="35px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2929 4.29289C12.6834 3.90237 13.3166 3.90237 13.7071 4.29289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L13.7071 19.7071C13.3166 20.0976 12.6834 20.0976 12.2929 19.7071C11.9024 19.3166 11.9024 18.6834 12.2929 18.2929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L12.2929 5.70711C11.9024 5.31658 11.9024 4.68342 12.2929 4.29289Z" fill="#606060"/>
+          </svg>
+         </span></Link>
       <div className='icons'>
         <img src="/resho.svg" alt="" width={50}/>
       </div>
       <img src="/resh.svg" alt="" className="resh" />
-      <img src="/doctor.svg" alt="" width={120} className="res" />
       <img src="/virus.svg" alt="" width={80} className="virus" />
 
         <div className=' data2'>
@@ -31,6 +35,7 @@ export default function Alaj() {
                     <h1>دواء {el.h2}</h1>
                    
                       <table>
+                      <img src="/doctor.svg" alt="" width={120} className="res" />
                         <tr><td>الاسم التجاري</td><td>{el.h1}</td> </tr>
                         <tr><td>تصنيف الدواء</td><td>{el.n2}</td></tr>
                         <tr><td>الفئة</td><td>{el.n3}</td></tr>
