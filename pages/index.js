032627d8833c453521262c22bcf43f0e2193c875
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-06-02 13:18:57 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -30,9 +30,7 @@ export default function Home() {
             <div class="rows ">
                 <div class="row">
                     <div class="col-12 col-md-6 col-lg-6 text-position">
-                        <figure class="covidImage">
-                            <img src="https://raw.githubusercontent.com/alaattinerby/Covid-19-Website/main/Covid-19/img/covid-image.png" class="covidImage__image" alt="Corona Virus"/>
-                        </figure>
+                       
                     </div>
                     <div class=" text-position">
                         <h1 class="coronavirus__title">What Is Covid-19</h1>
@@ -63,27 +61,21 @@ export default function Home() {
                 <div class="row cards ">
                     <div class="">
                         <div class="card">
-                            <figure class="card__figure">
-                                <img src="https://raw.githubusercontent.com/alaattinerby/Covid-19-Website/main/Covid-19/img/card1-image.png" class="card__image" width={100} alt=""/>
-                            </figure>
+                         
                             <h3 class="card__title">Air Transmission</h3>
                             <p class="card__desc"> Objectively evolve tactical before extensible initiatives. Efficiently simplify</p>
                         </div>
                     </div>
                     <div class="col-12 col-md-6 col-lg-4">
                         <div class="card">
-                            <figure class="card__figure">
-                                <img src="https://github.com/alaattinerby/Covid-19-Website/blob/main/Covid-19/img/card2-image.png?raw=true" class="card__image" alt=""/>
-                            </figure>
+                          
                             <h3 class="card__title">Human Contacts</h3>
                             <p class="card__desc">Washing your hands is one of thesimplest ways you can protect</p>
                         </div>
                     </div>
                     <div class="col-12 col-md-6 col-lg-4">
                         <div class="card">
-                            <figure class="card__figure">
-                                <img src="https://github.com/alaattinerby/Covid-19-Website/blob/main/Covid-19/img/card3-image.png?raw=true" class="card__image" alt=""/>
-                            </figure>
+                         
                             <h3 class="card__title">Contained Objects</h3>
                             <p class="card__desc">Use the tissue while sneezing,In this way, you can protect your droplets.</p>
                         </div>
