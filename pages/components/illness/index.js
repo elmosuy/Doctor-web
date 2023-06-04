439a0dab05d56b3102c0706c1376233bb7c033a7
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-06-05 00:24:12 +0300

    Add files via upload

--- pages/components/illness/index.js
+++ pages/components/illness/index.js
@@ -11,8 +11,12 @@ export default function Home() {
   const [select, setselect] = useState("")
   const [search, setsearcht] = useState("")
   const [idn, setidn] = useState("")
+  const [post, setpost] = useState([])
+  useEffect(() => { 
+    setpost(data)
+  }, []);
   
-  const filteredDaa = data.filter((el) => {
+  const filteredDaa = post.filter((el) => {
     return el.type.includes(select)&& el.link.includes(search)
   
   }
@@ -85,7 +89,7 @@ export default function Home() {
         <span className='right' onClick={handelillenss}><svg width="35px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M12.2929 4.29289C12.6834 3.90237 13.3166 3.90237 13.7071 4.29289L20.7071 11.2929C21.0976 11.6834 21.0976 12.3166 20.7071 12.7071L13.7071 19.7071C13.3166 20.0976 12.6834 20.0976 12.2929 19.7071C11.9024 19.3166 11.9024 18.6834 12.2929 18.2929L17.5858 13H4C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11H17.5858L12.2929 5.70711C11.9024 5.31658 11.9024 4.68342 12.2929 4.29289Z" fill="#606060"/>
           </svg>
-</span>
+         </span>
         <img src={data2[idn||1].image} alt="" width={800} />
         <h3>
           {data2[idn||1].header}
