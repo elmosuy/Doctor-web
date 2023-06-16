f9143221bc0bcd77ad4385b0bda8fcfe172e75de
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-06-16 05:47:12 +0300

    Update index.js

--- pages/components/doa/index.js
+++ pages/components/doa/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import data from "../../api/links.json"
+import data from "../../api/links2.json"
 import Link from 'next/link'
 
 const Alaj = () => {
