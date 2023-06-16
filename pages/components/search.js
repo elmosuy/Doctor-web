c4823e18c0ccc2eaa89a68125c3b39f13ae44eb5
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-06-16 05:30:36 +0300

    Add files via upload

--- pages/components/search.js
+++ pages/components/search.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import dataSearch from "../api/links.json"
+import Link from 'next/link';
+
 
 const Search = () => {
+  const [search, setsearch] = useState("da");
+
+  const handelremove = () => {
+    const search = document.getElementById("searcher");
+    search.classList.remove("show-search");
+  };
+  const filteredData = dataSearch.filter((el) => {
+   
+    return el.link.toLowerCase().includes(search)})
+
+
+  const handelsearch = () => {
+    const search = document.getElementById("searcher");
+    search.classList.add("show-search");
+  };
+
   return (
-    <div><div class="container">
-    <input placeholder="ابحث " required="" class="input" name="text" type="text"/>
+    <div><div class="container" onClick={handelsearch}>
+    <input placeholder="ابحث " required="" class="input" name="text" type="text"  onChange={(e) => {setsearch(e.target.value.toLowerCase()) }}/>
+           
     <div class="icon">
         <svg viewBox="0 0 512 512" class="ionicon" xmlns="http://www.w3.org/2000/svg">
             <title>Search</title>
@@ -11,7 +31,19 @@ const Search = () => {
             <path d="M338.29 338.29L448 448" stroke-width="32" stroke-miterlimit="10" stroke-linecap="round" stroke="currentColor" fill="none"></path>
         </svg>
     </div>
-</div></div>
+</div>
+
+<div className='searcher' id='searcher'>
+  {
+    filteredData.map((el)=>(
+      
+        <Link href={`/components/doa/${el.id}`}><div key={el.id} onClick={handelremove} className='search-item'><h2>{el.link}</h2> <p>{el.title}</p></div></Link>
+      
+    ))
+  }
+</div>
+
+</div>
   )
 }
 
